b248d9423f1ad670c060e9db98b372d6a56b261d
Shelo <correodeshelo@gmail.com>
2022-03-04 18:50:58 -0300

    Ranger: Plugin para extraer y comprimir ficheros

--- .config/ranger/install_plugins.sh
+++ .config/ranger/install_plugins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo Installing ranger plugins...
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+git clone https://github.com/maximtrp/ranger-archives.git ~/.config/ranger/plugins/ranger-archives
 echo Done
