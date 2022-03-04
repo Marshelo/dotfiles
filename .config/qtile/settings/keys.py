271e9d48d762fffebeb7a908d32f8a9add485e06
Shelo <correodeshelo@gmail.com>
2022-03-03 21:22:24 -0300

    Seguir más tarde

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -191,8 +191,10 @@ for i, group in enumerate(groups):
     if sep == 2:
         if ii - first_nums - len(group_keys) < common_first_nums:
             current_key = str(ii - len(group_keys) + 1)
-        # elif ii < first_nums + len(group_keys):
-        #     current_key = group_keys[ii - len(group_keys)]
+        # elif ii < first_nums + len(group_keys) + common_first_nums + common_group_keys:
+        #     # current_key = group_keys[ii - first_nums - len(group_keys) - common_first_nums]
+        #     pass
+
         # elif first_nums + len(group_keys) - 1 < ii < 9 + len(group_keys):
         #     current_key = str(ii - len(group_keys) + 1)
         # elif ii == 9 + len(group_keys):
