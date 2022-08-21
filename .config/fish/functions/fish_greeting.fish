293f41f967d02c842146e4b325aa8dc7d39c3288
Shelo <correodeshelo@gmail.com>
2022-08-21 16:22:55 -0400

    Empezó la configuración de kitty y el starship

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,14 +1,123 @@
 # Defined in /usr/share/fish/functions/fish_greeting.fish @ line 1
 function fish_greeting
     if not set -q fish_greeting
-        set option (random 1 3)
-        switch $option
+        set messages "chanchulín" "chuchetumare" "poto" "???" "!!!" "wena po olvidona"
+        set message $messages[(random 1 (count $messages))]
+        # set style (random 1 4)
+        set style (random 1 9)
+        switch $style
             case 1
-                toilet "Welcome" -w 28 -f pagga -F border | lolcat
+                cowthink $message
             case 2
-                cowthink "???"
+                toilet $message -f pagga -F border | lolcat
             case 3
-                toilet "Welcome" -w 28 -f pagga -F border
+                toilet $message -f pagga -F border # use for limit wide -w 28
+            case 4 # Bandera de chile con dialogo
+                set messages "Wena chuchetumare" "wena watón"
+                set message $messages[(random 1 (count $messages))]
+
+                echo
+                set_color normal -b normal
+                echo "           ($message)"
+
+                set_color blue
+                echo -n "  █"
+                set_color white -b blue
+                echo -n " "
+                set_color blue
+                echo -n
+                set_color white
+                echo -n "█████"
+                set_color normal -b normal
+                echo " /"
+
+                set_color red -b normal
+                echo "  ████████"
+            case 5 # Bandera de chile gritando
+                set messages "Wena chuchetumare" "C-H-III, L-EEE"
+                set message $messages[(random 1 (count $messages))]
+
+                echo
+                set_color normal -b normal
+                echo "          ¡¡$message!!"
+
+                set_color blue
+                echo -n "  █"
+                set_color white -b blue
+                echo -n " "
+                set_color blue
+                echo -n
+                set_color white
+                echo -n "█████"
+                set_color normal -b normal
+                echo " /"
+
+                set_color red -b normal
+                echo "  ████████"
+
+            case 6 # Dialogo de bandera con respuesta
+                # messages and answers arrays must have the same index length
+                set messages "CHORISOPAIPA" "Wena po"
+                set answers "NADA MEJOR QUE UN CHORIPAN METIO EN UNA SOPAIPA" "Wena po olvidona"
+                set randomNum (random 1 (count $messages))
+                set message $messages[$randomNum]
+                set answer $answers[$randomNum]
+
+                echo
+                set_color normal -b normal
+                echo "           ($message)"
+
+                set_color blue
+                echo -n "  █"
+                set_color white -b blue
+                echo -n " "
+                set_color blue
+                echo -n
+                set_color white
+                echo -n "█████"
+                set_color normal -b normal
+                echo " /"
+
+                set_color red -b normal
+                echo "  ████████"
+                echo
+
+                set_color white -b normal
+                toilet -f future " $answer"
+
+            case 7 # Grito de bandera con respuesta
+                # messages and answers arrays must have the same index length
+                set messages "C-H-III, L-EEE" "C-H-III, L-EEE"
+                set answers "CHIIIII, LEEEEE." "CHI CHI CHI, LE LE LE, VIVA CHILE"
+                set randomNum (random 1 (count $messages))
+                set message $messages[$randomNum]
+                set answer $answers[$randomNum]
+
+                echo
+                set_color normal -b normal
+                echo "          ¡¡$message!!"
+
+                set_color blue
+                echo -n "  █"
+                set_color white -b blue
+                echo -n " "
+                set_color blue
+                echo -n
+                set_color white
+                echo -n "█████"
+                set_color normal -b normal
+                echo " /"
+
+                set_color red -b normal
+                echo "  ████████"
+                echo
+
+                set_color white -b normal
+                toilet -f future " $answer"
+            case 8
+
+            case 9
+
         end
     end
 
